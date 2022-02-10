ae3cf26e9f639f08642b9eb9bd0c4a1efed10dd3
Roboskeletron <wwwrt287@gmail.com>
2022-02-10 20:15:54 +0300

    app context relocated

---
+++ Server/Models/AppContext.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Server.Services;
+
+namespace Server.Models
+{
+    public class AppContext : ApplicationContext
+    {
+        private NotifyIcon trayIcon;
+
+        public AppContext()
+        {
+            ContextMenuStrip menu = CreateContextMenu();
+
+            trayIcon = new NotifyIcon()
+            {
+                Icon = new Icon(Assembly.GetExecutingAssembly().GetManifestResourceStream("Server.Resources.HomeServer.ico")),
+                ContextMenuStrip = menu,
+                Visible = true,
+                Text = "Server"
+            };
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            Beacon.Port = Services.Server.Port;
+            Beacon.ScanerName = Scanner.GetScaner();
+            Beacon.Start(7778, 1000);
+            Services.Server.Start();
+        }
+
+        void Exit(object sender, EventArgs e)
+        {
+            trayIcon.Visible = false;
+
+            Application.Exit();
+        }
+        
+        private ContextMenuStrip CreateContextMenu()
+        {
+            ContextMenuStrip menu = new ContextMenuStrip();
+            ToolStripMenuItem item = new ToolStripMenuItem("Quit", null, Exit, "quit");
+            menu.Items.Add(item);
+            return menu;
+        }
+    }
+}
--- Server/Program.cs
+++ Server/Program.cs
@@ -1,61 +1,14 @@
 ﻿using System;
-using System.Drawing;
-using System.Text;
 using System.Windows.Forms;
 
 namespace Server
 {
     static class Program
     {
-        public static bool IsRun { get; set; } = true;
-
-        [STAThread]
+        [MTAThread]
         static void Main()
         {
-            Application.Run(new AppContext());
-            //Beacon.ScanerName = Scaner.GetScaner();
-            ////Beacon.ScanerName = "some scanner";
-            //Server.Port = 7777;
-            //Beacon.Port = Server.Port;
-            //Beacon.Start(7778, 1000);
-            //Server.Start();
-            //while (IsRun)
-            //{
-            //    string input = Console.ReadLine();
-            //    if (input == "exit")
-            //        break;
-            //}
-        }
-    }
-
-    public class AppContext : ApplicationContext
-    {
-        private NotifyIcon trayIcon;
-
-        public AppContext()
-        {
-            trayIcon = new NotifyIcon()
-            {
-                Icon = new System.Drawing.Icon(SystemIcons.WinLogo, 40, 40),
-                ContextMenu = new ContextMenu(new MenuItem[] {
-                new MenuItem("Exit", Exit)
-            }),
-                Visible = true
-            };
-
-            Beacon.ScanerName = Scanner.GetScaner();
-            //Beacon.ScanerName = "some scanner";
-            Server.Port = 7777;
-            Beacon.Port = Server.Port;
-            Beacon.Start(7778, 1000);
-            Server.Start();
-        }
-
-        void Exit(object sender, EventArgs e)
-        {
-            trayIcon.Visible = false;
-
-            Application.Exit();
+            Application.Run(new Models.AppContext());
         }
     }
 }
